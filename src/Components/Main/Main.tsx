c49a2524bd62068d2af410ba77e263bb2ec160da
Pavel Lebedev <pashoktver90@gmail.com>
2022-02-09 16:48:32 +0300

    update main

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -9,12 +9,20 @@ const Main = () => {
             <div className="content">
                 <div className={classes.main}>
                     <div className={classes.description}>
-                        <p>Привет</p>
-                        <p>Меня зовут Павел Лебедев.</p>
-                        <p>Я front-end разработчик</p>
+                        <h1>Я <br/>Павел Лебедев.</h1>
+                        <p><span>frontend developer</span></p>
+                        <p>
+                            Я  фронтенд-разработчик из Твери,
+                            занимаюсь созданием чистого и удобного интерфейса.
+                            Я увлечен созданием отличного программного обеспечения,
+                            которое улучшает жизнь окружающих меня людей.
+                        </p>
                     </div>
                     <div className={classes.ava}>
                         <img src={photoCV} alt="photo"/>
+                    </div>
+                    <div className={classes.avaBG}>
+
                     </div>
                 </div>
             </div>
